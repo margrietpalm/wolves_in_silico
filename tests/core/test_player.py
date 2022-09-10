f35605164a8b871a3d4e1df5900a5476f2d82e19
Margriet Palm <m.palm@electricant.com>
2022-09-10 23:07:47 +0200

    Add ci

---
+++ tests/core/test_player.py
@@ -0,0 +1,21 @@
+import pytest
+
+from wolves_in_silico.core.player import Role, Player
+
+
+def test_wolf():
+    wolf = Player(role=Role.WOLF, id=0)
+    assert wolf.is_wolf
+    assert str(wolf) == 'wolf_00'
+
+
+@pytest.mark.parametrize('major', (True, False))
+def test_civ(major):
+    civ = Player(role=Role.CIV, id=0)
+    civ.is_major = major
+    assert not civ.is_wolf
+    if major:
+        assert str(civ) == 'civ_00*'
+    else:
+        assert str(civ) == 'civ_00'
+
